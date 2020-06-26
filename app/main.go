d4dce3a554bc9eeb83eb8529588c8614486a7b45
violaTang <1055246076@qq.com>
2020-06-26 20:08:20 +0800

    add app initAppEngine

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 
 	"github.com/ViolaTangxl/l-bridge/config"
+	"github.com/ViolaTangxl/l-bridge/env"
 	"github.com/sirupsen/logrus"
 )
 
@@ -13,9 +14,21 @@ func main() {
 	flag.StringVar(&configPath, "conf", "config.yml", "config file path")
 	flag.Parse()
 
-	_, err := config.ParseConfig(configPath)
+	conf, err := config.ParseConfig(configPath)
 	if err != nil {
 		logrus.WithField("err", err).Fatal("failed to load config")
+		return
+	}
+
+	app, err := env.InitAppEngine(logrus.StandardLogger(), conf)
+	if err != nil {
+		logrus.WithField("err", err).Fatal("failed to init engine")
+		return
+	}
+
+	err = app.Run(conf.Server.Port)
+	if err != nil {
+		logrus.Errorf("l-bridge service run with err, err: %s", err)
 		return
 	}
 }
